694fe2c89876f8cf9cdaa9e4f68880fd2a95515f
igor.okunev@gmail.com <igor.okunev@gmail.com@0e5d73fd-e8b7-04b0-fbc2-0c9e88db4991>
2011-02-08 10:04:04 +0000

    CSS update

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -1,18 +1,16 @@
 <head>
   <title>Asterisk Call Detail Records</title>
-  <meta name="GENERATOR" content="Quanta Plus" />
   <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
 <link rel="stylesheet" href="style/screen.css" type="text/css" media="screen" />
-<!--<link rel="stylesheet" href="style/print.css" type="text/css" media="print" />-->
 <link rel="shortcut icon" href="templates/mss/images/favicon.ico" />
 </head>
 <body>
 <table id="header">
   <tr>
-    <td id="header_logo" rowspan="2"><a href="/" title="Home"><img src="templates/mss/images/asterisk.gif" alt="Asterisk CDR Viewer" /></a></td>
-    <td id="header_title">Messinet Secure Services</td>
+    <td id="header_logo" rowspan="2" align="left"><a href="/" title="Home"><img src="templates/mss/images/asterisk.gif" alt="Asterisk CDR Viewer" /></a></td>
+    <td id="header_title">Asterisk CDR Viewer</td>
   </tr>
   <tr>
-    <td id="header_subtitle">WebCDR - Call Detail Records</td>
+    <td id="header_subtitle"><!--Call Detail Records--></td>
   </tr>
 </table>
