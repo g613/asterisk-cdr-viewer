a7918121cae3e323bcc66ddb1d118f304d597a69
igor.okunev@gmail.com <igor.okunev@gmail.com@0e5d73fd-e8b7-04b0-fbc2-0c9e88db4991>
2011-02-12 17:25:35 +0000

    new styles
    added concurent call report

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -2,12 +2,12 @@
   <title>Asterisk Call Detail Records</title>
   <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
 <link rel="stylesheet" href="style/screen.css" type="text/css" media="screen" />
-<link rel="shortcut icon" href="templates/mss/images/favicon.ico" />
+<link rel="shortcut icon" href="templates/images/favicon.ico" />
 </head>
 <body>
 <table id="header">
   <tr>
-    <td id="header_logo" rowspan="2" align="left"><a href="/" title="Home"><img src="templates/mss/images/asterisk.gif" alt="Asterisk CDR Viewer" /></a></td>
+    <td id="header_logo" rowspan="2" align="left"><a href="/" title="Home"><img src="templates/images/asterisk.gif" alt="Asterisk CDR Viewer" /></a></td>
     <td id="header_title">Asterisk CDR Viewer</td>
   </tr>
   <tr>
