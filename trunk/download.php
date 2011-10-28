bc054852f8e6ee18fe17e78b30125314928d7375
igor.okunev@gmail.com <igor.okunev@gmail.com@0e5d73fd-e8b7-04b0-fbc2-0c9e88db4991>
2011-10-28 09:26:42 +0000

    improved content-type detection
    :wq

--- trunk/download.php
+++ trunk/download.php
@@ -6,23 +6,51 @@ header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-ch
 header('Pragma: no-cache');
 
 if (isset($_GET['audio'])) {
-  header("Content-Type: application/$system_audio_format");
-  header('Content-Transfer-Encoding: binary');
-  header('Content-Length: '.filesize("$system_monitor_dir/$_GET[audio]"));
-  header("Content-Disposition: attachment; filename=\"$_GET[audio]\"");
-  readfile("$system_monitor_dir/$_GET[audio]");
+	$extension = strtolower(substr(strrchr($_GET[audio],"."),1));
+	$ctype ='';
+	switch( $extension ) {
+		case "wav16":
+			$ctype="audio/x-wav";
+			break;
+		case "wav":
+			$ctype="audio/x-wav";
+			break;
+		case "ulaw":
+			$ctype="audio/basic";
+			break;
+		case "alaw":
+			$ctype="audio/x-alaw-basic";
+			break;
+		case "sln":
+			$ctype="audio/x-wav";
+			break;
+		case "gsm":
+			$ctype="audio/x-gsm";
+			break;
+		case "g729":
+			$ctype="audio/x-g729";
+			break;
+		default: 
+			$ctype="application/$system_audio_format";
+			break ;
+	}
+	header("Content-Type: $ctype");
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Length: '.filesize("$system_monitor_dir/$_GET[audio]"));
+	header("Content-Disposition: attachment; filename=\"$_GET[audio]\"");
+	readfile("$system_monitor_dir/$_GET[audio]");
 } elseif (isset($_GET['fax'])) {
-  header('Content-Type: image/tiff');
-  header('Content-Transfer-Encoding: binary');
-  header('Content-Length: '.filesize("$system_fax_archive_dir/$_GET[fax]"));
-  header("Content-Disposition: attachment; filename=\"$_GET[fax]\"");
-  readfile("$system_fax_archive_dir/$_GET[fax]");
+	header('Content-Type: image/tiff');
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Length: '.filesize("$system_fax_archive_dir/$_GET[fax]"));
+	header("Content-Disposition: attachment; filename=\"$_GET[fax]\"");
+	readfile("$system_fax_archive_dir/$_GET[fax]");
 } elseif (isset($_GET['csv'])) {
-  header('Content-Type: text/csv');
-  header('Content-Transfer-Encoding: binary');
-  header('Content-Length: '.filesize("/tmp/$_GET[csv]"));
-  header("Content-Disposition: attachment; filename=\"$_GET[csv]\"");
-  readfile("$system_tmp_dir/$_GET[csv]");
+	header('Content-Type: text/csv');
+	header('Content-Transfer-Encoding: binary');
+	header('Content-Length: '.filesize("/tmp/$_GET[csv]"));
+	header("Content-Disposition: attachment; filename=\"$_GET[csv]\"");
+	readfile("$system_tmp_dir/$_GET[csv]");
 }
 
 exit();
