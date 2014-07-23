132a68723a6436430ce9b10c756da6ec88a2625e
igor.okunev@gmail.com <igor.okunev@gmail.com@0e5d73fd-e8b7-04b0-fbc2-0c9e88db4991>
2014-07-23 19:02:26 +0000

    some changes for https://code.google.com/p/asterisk-cdr-viewer/issues/detail?id=23

--- trunk/templates/footer.tpl.php
+++ trunk/templates/footer.tpl.php
@@ -1,6 +1,6 @@
 <div id="footer">
 <p>
-	<a href="http://code.google.com/p/asterisk-cdr-viewer/">Asterisk CDR viewer ( v.1.0.4 )</a>
+	<a href="http://code.google.com/p/asterisk-cdr-viewer/">Asterisk CDR viewer ( v.1.0.5 )</a>
     <!-- by Igor Okunev ( igor.okunev [at] gmail.com ) -->
 </p>
 </div>
